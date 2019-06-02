f8346fb7f418be6cf4d5dcf86b06deabcdc3535d
Kohei Nozaki <kohei@apache.org>
2019-06-02 15:10:47 +0900

    wip

--- mysql/wait.sh
+++ mysql/wait.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-until echo select 1 | mysql -h localhost -uroot -pmy-secret-pw --protocol tcp --port=3306; do sleep 2s; done
+until echo select 1 | mysql -h localhost -uroot -pmy-secret-pw --protocol tcp --port=3306 > /dev/null 2>&1; do echo 'Waiting for MySQL...'; sleep 2s; done
+echo 'MySQL should be running now'
